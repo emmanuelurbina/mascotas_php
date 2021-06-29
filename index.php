0ad4016a14bf3f4483945f8ec7caaba519546c0c
Emmanuel Lucio Urbina <emmanuelluur@outlook.com>
2021-06-29 13:36:11 -0500

    Usuario Crud

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <?php
 date_default_timezone_set('US/Central');
 require_once './animal.php';
-
-$animal = new Animal();
-
-$animal->name = 'Thor';
-$animal->specie = 'Gato';
-$animal->breed = 'Gato';
-$animal->genre = 'Macho';
-$animal->color = 'Atigrado';
-$animal->age = 4;
-
-//$animal->create();
-
-$results = $animal->get_all();
-echo json_encode($results);
-$animal->id = 2;
-$animal->name = 'Salem';
-$animal->update();
+require_once './user.php';
 
 
-
-$results = $animal->get_all();
+$results = $user->get_all();
 echo json_encode($results);
---
+++ user.php
@@ -0,0 +1,55 @@
+<?php
+require_once './crud.php';
+class User extends Crud
+{
+  private $pdo;
+  public $id;
+  public $name;
+  public $lastName;
+  public $address;
+  public $phone;
+  public $sex;
+  public $age;
+  const TABLE = 'user';
+  public function __construct()
+  {
+    parent::__construct(self::TABLE);
+    $this->pdo = parent::connect();
+  }
+  public function create()
+  {
+    try {
+      //code...
+      $stm = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (name, lastName, address, phone, sex, age) VALUES (?,?,?,?,?,?)");
+      $stm->execute(array(
+        $this->name,
+        $this->lastName,
+        $this->address,
+        $this->phone,
+        $this->sex,
+        $this->age));
+    } catch (\PDOException $e) {
+      //throw $e;
+      echo $e->getMessage();
+    }
+  }
+  public function update()
+  {
+    try {
+      //code...
+      $stm = $this->pdo->prepare("UPDATE " . self::TABLE . " SET name=?, lastName=?, address=?, phone=?, sex=?, age=? WHERE id=?");
+      $stm->execute(array(
+        $this->name,
+        $this->lastName,
+        $this->address,
+        $this->phone,
+        $this->sex,
+        $this->age,
+        $this->id
+      ));
+    } catch (\PDOException $e) {
+      //throw $e;
+      echo $e->getMessage();
+    }
+  }
+}
